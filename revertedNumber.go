03c72236b337338627c0c0a0906b827d1d1e957d
wangzaidada <abcduan@outlook.com>
2025-07-20 15:12:34 +0800

    新增注释

--- revertedNumber.go
+++ revertedNumber.go
@@ -1,7 +1,7 @@
 package main
 import "fmt"
 func isPalindrome(x int) bool {
-    if x <0 || (x % 10 ==0 && x != 0){
+    if x <0 || (x % 10 ==0 && x != 0){ // 负数必不回文，非零的零结尾必不回文
         return false
     }
 
@@ -10,7 +10,7 @@ func isPalindrome(x int) bool {
         revertedNumber = x % 10 + revertedNumber * 10
         x /= 10
     }
-    return x == revertedNumber || x == revertedNumber / 10
+    return x == revertedNumber || x == revertedNumber / 10 // 偶数位会直接相等，奇数位在/10 之后也会相等
 }
 func main(){
 	x :=  12321
