0fe1d1cd4f4c2e4e2650eabd17ce8765e3c77f3d
wangzaidada <abcduan@outlook.com>
2025-07-27 13:13:35 +0800

    详细解释，资源互斥访问

--- routine.go
+++ routine.go
@@ -7,24 +7,24 @@ import (
 
 // 原子操作
 var total struct { // 声明了一个锁结构体（匿名写法）
-	sync.Mutex
-	value int
+	sync.Mutex //加锁确保资源访问互斥
+	value      int
 }
 
 func Worker(wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer wg.Done() // 确保函数退出时通知等待组
 	for i := 0; i < 100; i++ {
-		total.Lock()
+		total.Lock() // 资源访问互斥
 		total.value += 1
 		total.Unlock()
 	}
 }
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(2)
+	var wg sync.WaitGroup // 等待组
+	wg.Add(2)             // 注册两个任务
+	go Worker(&wg)        // 启动任务，传入等待组指针
 	go Worker(&wg)
-	go Worker(&wg)
-	wg.Wait()
+	wg.Wait() // 阻塞等待所有注册的goroutine完成
 	fmt.Println(total.value)
 }
