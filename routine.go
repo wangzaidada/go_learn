f6c14687d49a553dec8fec9a5fa9a638e19250bd
wangzaidada <abcduan@outlook.com>
2025-07-27 14:17:34 +0800

    添加更轻量的原子操作实现方式

--- routine.go
+++ routine.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 // 原子操作
@@ -20,11 +21,27 @@ func Worker(wg *sync.WaitGroup) {
 	}
 }
 
+// sync/atomic 实现原子操作
+var total_2 uint64
+
+func Worker_2(wg *sync.WaitGroup) {
+	defer wg.Done()
+	var i uint64 // 将i 定义为无符号int64类型
+	for i = 0; i < 100; i++ {
+		atomic.AddUint64(&total_2, 1) // 更轻量级的互斥访问
+	}
+}
 func main() {
 	var wg sync.WaitGroup // 等待组
 	wg.Add(2)             // 注册两个任务
 	go Worker(&wg)        // 启动任务，传入等待组指针
 	go Worker(&wg)
 	wg.Wait() // 阻塞等待所有注册的goroutine完成
+	var wg_2 sync.WaitGroup
+	wg_2.Add(2)
+	go Worker_2(&wg_2)
+	go Worker_2(&wg_2)
+	wg_2.Wait()
 	fmt.Println(total.value)
+	fmt.Println(total_2)
 }
