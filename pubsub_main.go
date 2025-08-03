4189a316c3c14f368adf1227e4ea77e73e5faf65
wangzaidada <abcduan@outlook.com>
2025-08-03 13:00:52 +0800

    发布订阅者模型调用实例

--- pubsub_main.go
+++ pubsub_main.go
@@ -5,30 +5,30 @@ import (
 	"strings"
 	"time"
 
-	utils "pubsub.go/pubsub"
+	pubsub "pubsub.go/pubsub"
 )
 
 func main() {
-	p := utils.NewPublisher(100*time.Microsecond, 10)
+	p := pubsub.NewPublisher(100*time.Microsecond, 10)
 	defer p.Close()
 
-	all := p.Subscribe()
-	golong := p.SubscribeTopic(func(v interface{}) bool {
-		if s, ok := v.(string); ok {
-			return strings.Contains(s, "golong")
+	all := p.Subscribe()                                  // 订阅全部内容
+	golong := p.SubscribeTopic(func(v interface{}) bool { //传入消息过滤判断器
+		if s, ok := v.(string); ok { // 如果消息类型是字符型
+			return strings.Contains(s, "golong") // 订阅 golong true, 其他 false
 		}
-		return false
+		return false // 不订阅  false
 	})
 	p.Publish("hello, world!")
 	p.Publish("hello, golong!")
-	go func() {
-		for msg := range all {
-			fmt.Println("all:", msg)
+	go func() { // 启动 匿名函数
+		for msg := range all { // 持续从通道中获取数据
+			fmt.Println("all:", msg) // 输出数据
 		}
 	}()
 
 	go func() {
-		for msg := range golong {
+		for msg := range golong { // 持续从通道中获取数据
 			fmt.Println("golong:", msg)
 		}
 	}()
